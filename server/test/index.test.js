621260f7cf368afe44cde9fb6a37b1a35ac21b87
PHOOMCHANOK CHAYANON <77683049+OniiChan00@users.noreply.github.com>
2023-04-22 18:05:58 +0700

    Update index.test.js

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,62 +1,21 @@
-const chai = require("chai");
-const expect = chai.expect;
-const chaiHttp = require("chai-http");
-const app = require("../index.js");
-const jwt = require("jsonwebtoken");
 const SECRET_KEY = "nDXuLl0TlpIG3o1JkXGSoNcQos7pVh7k";
+const request = require("supertest");
+const app = require("./index.js");
+const jwt = require("jsonwebtoken");
 
-chai.use(chaiHttp);
 
-describe("Test API", () => {
-  it("should return a message", (done) => {
-    chai
-      .request(app)
-      .get("/test")
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.msg).to.equal("test");
-        done();
-      });
+describe("Test get token", () => {
+  test("should return a JWT token", async () => {
+    const res = await request(app).get("/testtoken").expect(200);
+    console.log(res.text);
+    const token = res.text
+    // const json = res.text
+    // const obj = JSON.parse(json)
+    // const token = obj.token
+    const decoded = jwt.verify(token, SECRET_KEY);
+    expect(decoded.user).toBe("phoo");
   });
 });
-
-// describe("check vaild token", () => {
-//   it("return true", (done) => {
-//     const username = "admin";
-//     const password = "admin";
-//     const token = jwt.sign({ username, password }, SECRET_KEY);
-//     console.log(token);
-//     chai
-//       .request(app)
-//       .post("/checktoken")
-//       .send({ token: token })
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         console.log(res);
-//         expect(res.body).to.equal("true");
-//         done();
-//       });
-//   });
-// });
-
-
-// describe("create token", () => {
-//   it("return true", (done) => {
-//     const username = "admin";
-//     const password = "admin";
-//     chai
-//       .request(app)
-//       .post("/createToken")
-//       .send({ admin: username, password: password })
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         console.log(res);
-//         expect(res.body).to.equal("true");
-//         done();
-//       });
-//   });
-// });
-
 
 
 

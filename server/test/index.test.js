16c70cd2767581477d24ca79fda9d5df0d8eb2ef
PHOOMCHANOK CHAYANON <77683049+OniiChan00@users.noreply.github.com>
2023-04-22 18:33:16 +0700

    Update index.test.js

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,22 +1,21 @@
-const chai = require("chai");
-const expect = chai.expect;
-const chaiHttp = require("chai-http");
-const app = require("../index.js");
+const request = require("supertest");
+const app = require("..//index.js");
 const jwt = require("jsonwebtoken");
+
 const SECRET_KEY = "nDXuLl0TlpIG3o1JkXGSoNcQos7pVh7k";
 
-chai.use(chaiHttp);
 
-describe("Test API", () => {
-  it("should return a message", (done) => {
-    chai
-      .request(app)
-      .get("/test")
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.msg).to.equal("test");
-        done();
-      });
+
+describe("GET /testtoken", () => {
+  test("should return a JWT token", async () => {
+    const res = await request(app).get("/testtoken").expect(200);
+    console.log(res.text);
+    const token = res.text
+    // const json = res.text
+    // const obj = JSON.parse(json)
+    // const token = obj.token
+    const decoded = jwt.verify(token, secret_key);
+    expect(decoded.user).toBe("phum");
   });
 });
 
